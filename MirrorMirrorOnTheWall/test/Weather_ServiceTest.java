1347db86b92c475afd74a2dda412f8450c5090a4
Melissa Mulcahy <melmulcahy32@gmail.com>
2017-11-18 22:20:04 -0500

    Working on API Service calls. They build and return stuff, but thrown an error when getting actual temperatures. Found that we only get 50 calls per day so I can no longer test. Committing and pushing for the night.

---
+++ MirrorMirrorOnTheWall/test/Weather_ServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author melmulcahy
+ */
+public class Weather_ServiceTest {
+    
+    public Weather_ServiceTest() {
+    }
+
+    /**
+     * Test of GetDailyForecast method, of class Weather_Service.
+     */
+    @Test
+    public void testGetDailyForecast() {
+        System.out.println("GetDailyForecast");
+        Weather_Service instance = new Weather_Service();
+        HashMap<String,String> result = instance.GetDailyForecast();
+        assertThat(result, not(new HashMap()));
+    }
+
+    /**
+     * Test of GetWeeklyForecast method, of class Weather_Service.
+     */
+    @Test
+    public void testGetWeeklyForecast() {
+        System.out.println("GetWeeklyForecast");
+        Weather_Service instance = new Weather_Service();
+        String expResult = "";
+        ArrayList result = instance.GetWeeklyForecast();
+        assertThat(result, not(new ArrayList<HashMap>()));
+    }
+
+    /**
+     * Test of GetHourlyForecast method, of class Weather_Service.
+     */
+    @Test
+    public void testGetHourlyForecast() {
+        System.out.println("GetHourlyForecast");
+        Weather_Service instance = new Weather_Service();
+        String expResult = "";
+        ArrayList<HashMap> result = instance.GetHourlyForecast();
+        assertThat(result, not(new ArrayList<HashMap>()));
+    }
+    
+}
